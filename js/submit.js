bdd955d850f9f2044aa9d892f97232da00b2c87f
Lawrence Humphrey <lawrencehumphrey@Lawrences-MacBook-Pro-2.local>
2015-02-17 16:34:32 -0600

    david zmith can suck it

--- js/submit.js
+++ js/submit.js
@@ -14,6 +14,28 @@ $(document).ready(function() {
             phone       : rental[3].value
         });
     }
+    
+    // Add a callback that is triggered for each chat message.
+    myDataRef.limitToLast(10).on('child_added', function (snapshot) {
+        //GET DATA
+        var data = snapshot.val();
+        
+        var date        = data.date;      
+        var email       = data.email;     
+        var first_name  = data.first_name;
+        var last_name   = data.last_name; 
+        var phone       = data.phone;     
             
-    $('#butts').on('click', submit);   
+        //CREATE ELEMENTS MESSAGE & SANITIZE TEXT
+
+        var logelement = $("<li>");
+        
+        $(logelement).append("<p>first name: " + first_name + " </p>");
+        $(logelement).append("<p>last name: " + last_name + " </p>");
+        $(logelement).append("<p>date: " + date + " </p>");
+        $(logelement).append("<p>phone: " + phone + " </p>");
+        $(logelement).append("<p>email: " + email + " </p>");
+        
+        $(".log").prepend(logelement);
+    });
 });
