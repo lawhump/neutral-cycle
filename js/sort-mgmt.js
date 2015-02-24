540a124db15c09fc875094a4bd50f0c60e615162
Lawrence Humphrey <lawrencehumphrey@citeswpaurhprv-172-16-48-114.near.illinois.edu>
2015-02-23 19:34:24 -0600

    realtime sorting

--- js/sort-mgmt.js
+++ js/sort-mgmt.js
@@ -1,106 +1,69 @@
 $( document ).ready(function() {
-    var data = [];
-    
-    function reservation(date){
-        this.name = "Lawrence";
-        this.date = date;
-    }
-    
-    function parseDate(input) {
-        var parts = input.match(/(\d+)/g);
-        var int_parts = parts.map(function(item){return parseInt(item)});
-        return new Date(int_parts[0], int_parts[1], int_parts[2], int_parts[3], int_parts[4], int_parts[5]);
-    }
-    
-    /*
-    function sort_and_update(dates){
-        dates.sort(function(a, b) {
-            return a - b;
-        });
-        console.log(dates);
-        console.log("////////////////////////////");
-        var logelement = $("<li>");
-        $.each(dates, function(val){
-            $(logelement).append("<p>: " + this + " </p>");
-            console.log(this);
-        });
-        $("#log").prepend(logelement);
-    }
-    */
-    function sort_and_update(reservations){
-        reservations.sort(function(a, b) {
-            return Date.parse(a.date) - Date.parse(b.date);
-        });
-        console.log(reservations);
-        console.log("////////////////////////////");
-        var logelement = $("<li>");
-        $.each(reservations, function(val){
-            $(logelement).append("<p>name: " + this.name + " date: " + this.date + " p>");
-            console.log(this);
-        });
-        $("#log").prepend(logelement);
-    }
-    
-    
-    //function sort_and_update(data){
-    //    console.log(data);
-    //    data.sort(function(a, b) {
-    //        //console.log("a= " + a + " b= " + b);
-    //        return Date.parse(a.date) - Date.parse(b.date);
-    //    });
-    //    console.log("////////////////////////////");
-    //    var logelement = $("<li>");
-    //    $.each(data, function(){
-    //        $(logelement).append("<p>first name: " + first_name + " </p>");
-    //        $(logelement).append("<p>last name: " + last_name + " </p>");
-    //        $(logelement).append("<p>date: " + date + " </p>");
-    //        $(logelement).append("<p>phone: " + phone + " </p>");
-    //        $(logelement).append("<p>email: " + email + " </p>");
-    //        console.log(this);
-    //    });
-    //    $("#log").prepend(logelement);
-    //}
-    
-    
-    var elems = $.makeArray($(".dateDiv"));
-    var dates = [];
-    var res_objs = [];
-    
-    for(var i=0; i<elems.length; i++){
-        //console.log(elems[i].innerHTML);
-        dates.push(parseDate((elems[i].innerHTML).toString()));
-        res_objs.push(new reservation(dates[i]));
-        console.log(res_objs[i]);
-    }
-    
-    //sort_and_update(dates);
-    sort_and_update(res_objs);
-    
-    /**
-     * firebase shit; more like playing with fire ahhhhhhhh
-     */
+    //reference to the firebase location
     var myDataRef = new Firebase('https://neutral-cycle.firebaseio.com/');
     
-    //myDataRef.limitToLast(10).on('child_added', function (snapshot) {
-    //    //GET DATA
-    //    var _data = snapshot.val();
-    //    
-    //    var date        = _data.date;
-    //    var email       = _data.email;
-    //    var first_name  = _data.first_name;
-    //    var last_name   = _data.last_name; 
-    //    var phone       = _data.phone;
-    //    
-    //    data.push(_data);
-    //    console.log(_data);
-    //});
+    // Keep a mapping of firebase locations to HTML elements, so we can
+    // move / remove elements as necessary.
+    var htmlForPath = {};
+     
+    // Helper function that takes a new score snapshot and adds an appropriate row to our leaderboard table.
+    function handleReservationAdded(reservationSnapshot, prevReservationName) {
+        
+        var data = reservationSnapshot.val();
+        
+        var date        = data.date;      
+        var email       = data.email;     
+        var first_name  = data.first_name;
+        var last_name   = data.last_name; 
+        var phone       = data.phone;  
+        
+        // CREATE ELEMENTS MESSAGE & SANITIZE TEXT
+        
+        var logelement = $("<li>");
+        
+        $(logelement).append("<p>first name: " + first_name + " </p>");
+        $(logelement).append("<p>last name: " + last_name + " </p>");
+        $(logelement).append("<p>date: " + date + " </p>");
+        $(logelement).append("<p>phone: " + phone + " </p>");
+        $(logelement).append("<p>email: " + email + " </p>");
+        
+        // Store a reference to the table row so we can get it again later.
+        htmlForPath[reservationSnapshot.key()] = logelement;
+        
+        // Insert the new score in the appropriate place in the table.
+        if (prevReservationName === null) {
+            $(".log").append(logelement);
+        }
+        else {
+            var lowerScoreRow = htmlForPath[prevReservationName];
+            lowerScoreRow.before(logelement);
+        }
+    }
+    // Helper function to handle a reservation object being removed; just removes
+    // the corresponding table row.
+    function handleReservationRemoved(reservationSnapshot) {
+        var removedReservationRow = htmlForPath[reservationSnapshot.key()];
+        //logelement.remove();
+        delete htmlForPath[reservationSnapshot.key()];
+    }
     
-    //myDataRef.orderByValue().limitToLast(3).on("value", function(snapshot) {
-    //    snapshot.forEach(function(data) {
-    //        console.log("The " + data.key() + " date is " + data.val().date);
-    //    });
-    //});
+    var reservationListView = myDataRef.limitToLast(10);
     
-    //console.log(data);
-    //sort_and_update(data);
+    // Add a callback to handle when a new reservation is added.
+    reservationListView.on('child_added', function (newReservationSnapshot, prevReservationName) {
+        handleReservationAdded(newReservationSnapshot, prevReservationName);
+    });
+  
+    // Add a callback to handle when a reservation is removed
+    reservationListView.on('child_removed', function (oldReservationSnapshot) {
+      handleReservationRemoved(oldReservationSnapshot);
+    });
+    
+      // Add a callback to handle when a reservation changes or moves positions.
+      var changedCallback = function (reservationSnapshot, prevReservationName) {
+        handleReservationRemoved(reservationSnapshot);
+        handleReservationAdded(reservationSnapshot, prevReservationName);
+      };
+      reservationListView.on('child_moved', changedCallback);
+      reservationListView.on('child_changed', changedCallback);
 });
--- js/submit.js
+++ js/submit.js
@@ -2,7 +2,91 @@ $(document).ready(function() {
     //reference to the firebase location
     var myDataRef = new Firebase('https://neutral-cycle.firebaseio.com/');
     
+    // Keep a mapping of firebase locations to HTML elements, so we can
+    // move / remove elements as necessary.
+    var htmlForPath = {};
+     
+    // Helper function that takes a new score snapshot and adds an appropriate row to our leaderboard table.
+    function handleReservationAdded(reservationSnapshot, prevReservationName) {
+        
+        var data = reservationSnapshot.val();
+        
+        var date        = data.date;      
+        var email       = data.email;     
+        var first_name  = data.first_name;
+        var last_name   = data.last_name; 
+        var phone       = data.phone;  
+        
+        // CREATE ELEMENTS MESSAGE & SANITIZE TEXT
+        
+        var logelement = $("<li>");
+        
+        $(logelement).append("<p>first name: " + first_name + " </p>");
+        $(logelement).append("<p>last name: " + last_name + " </p>");
+        $(logelement).append("<p>date: " + date + " </p>");
+        $(logelement).append("<p>phone: " + phone + " </p>");
+        $(logelement).append("<p>email: " + email + " </p>");
+        
+        // Store a reference to the table row so we can get it again later.
+        htmlForPath[reservationSnapshot.key()] = logelement;
+        
+        // Insert the new score in the appropriate place in the table.
+        if (prevReservationName === null) {
+            $(".log").append(logelement);
+        }
+        else {
+            var lowerScoreRow = htmlForPath[prevReservationName];
+            lowerScoreRow.before(logelement);
+        }
+    }
+    // Helper function to handle a reservation object being removed; just removes
+    // the corresponding table row.
+    function handleReservationRemoved(reservationSnapshot) {
+        var removedReservationRow = htmlForPath[reservationSnapshot.key()];
+        //logelement.remove();
+        delete htmlForPath[reservationSnapshot.key()];
+    }
+    
+    var reservationListView = myDataRef.limitToLast(10);
+    
+    // Add a callback to handle when a new reservation is added.
+    reservationListView.on('child_added', function (newReservationSnapshot, prevReservationName) {
+        handleReservationAdded(newReservationSnapshot, prevReservationName);
+    });
+  
+    // Add a callback to handle when a reservation is removed
+    reservationListView.on('child_removed', function (oldReservationSnapshot) {
+      handleReservationRemoved(oldReservationSnapshot);
+    });
+    
+      // Add a callback to handle when a reservation changes or moves positions.
+      var changedCallback = function (reservationSnapshot, prevReservationName) {
+        handleReservationRemoved(reservationSnapshot);
+        handleReservationAdded(reservationSnapshot, prevReservationName);
+      };
+      reservationListView.on('child_moved', changedCallback);
+      reservationListView.on('child_changed', changedCallback);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    function generateKey(){
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for( var i=0; i < 5; i++ )
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        return text;
+    }
+    
     function submit(){
+        var key = generateKey();
         var valid = $('#rental').valid();
         
         //validate form
@@ -15,15 +99,8 @@ $(document).ready(function() {
         var rental = $("#rental").serializeArray();
         var datetime = new Date(rental[4].value + " " + rental[5].value + " CST");
 
-        //myDataRef.push({
-        //    date        : datetime.toString(),
-        //    email       : rental[2].value,
-        //    first_name  : rental[0].value,
-        //    last_name   : rental[1].value,
-        //    phone       : rental[3].value
-        //});
-        
-        myDataRef.setWithPriority({
+        var resRef = myDataRef.child(key);
+        resRef.setWithPriority({ 
             date        : datetime.toString(),
             email       : rental[2].value,
             first_name  : rental[0].value,
@@ -33,6 +110,7 @@ $(document).ready(function() {
     }
     
     // Add a callback that is triggered for each chat message.
+    /*
     myDataRef.limitToLast(10).on('child_added', function (snapshot) {
         //GET DATA
         var data = snapshot.val();
@@ -54,6 +132,7 @@ $(document).ready(function() {
         
         $(".log").prepend(logelement);
     });
+    */
     
     $("#butts").on("click", submit);
 });
