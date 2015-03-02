0d5c036c68cf325dc405285f7157cd7907cbbb57
Lawrence Humphrey <lawrencehumphrey@Lawrences-MacBook-Pro-2.local>
2015-03-02 11:21:54 -0600

    rebuilt in angular

--- js/sort-mgmt.js
+++ js/sort-mgmt.js
@@ -1,69 +1,12 @@
-$( document ).ready(function() {
-    //reference to the firebase location
-    var myDataRef = new Firebase('https://neutral-cycle.firebaseio.com/');
-    
-    // Keep a mapping of firebase locations to HTML elements, so we can
-    // move / remove elements as necessary.
-    var htmlForPath = {};
-     
-    // Helper function that takes a new score snapshot and adds an appropriate row to our leaderboard table.
-    function handleReservationAdded(reservationSnapshot, prevReservationName) {
-        
-        var data = reservationSnapshot.val();
-        
-        var date        = data.date;      
-        var email       = data.email;     
-        var first_name  = data.first_name;
-        var last_name   = data.last_name; 
-        var phone       = data.phone;  
-        
-        // CREATE ELEMENTS MESSAGE & SANITIZE TEXT
-        
-        var logelement = $("<tr>");
-        
-        $(logelement).append("<td>" + first_name + " </td>");
-        $(logelement).append("<td>" + last_name + " </td>");
-        $(logelement).append("<td>" + date + " </td>");
-        $(logelement).append("<td>" + phone + " </td>");
-        $(logelement).append("<td>" + email + " </td>");
-        
-        // Store a reference to the table row so we can get it again later.
-        htmlForPath[reservationSnapshot.key()] = logelement;
-        
-        // Insert the new score in the appropriate place in the table.
-        if (prevReservationName === null) {
-            $(".log").append(logelement);
-        }
-        else {
-            var lowerScoreRow = htmlForPath[prevReservationName];
-            lowerScoreRow.after(logelement);
-        }
+var myApp = angular.module("myApp", ["firebase"]);
+
+myApp.controller('MyController', ['$scope', '$firebase',
+    function($scope, $firebase) {
+        //CREATE A FIREBASE REFERENCE
+        var ref = new Firebase("https://neutral-cycle.firebaseio.com/");
+
+        // GET MESSAGES AS AN ARRAY
+        $scope.reservations = $firebase(ref).$asArray();
+        console.log($scope.reservations);
     }
-    // Helper function to handle a reservation object being removed; just removes
-    // the corresponding table row.
-    function handleReservationRemoved(reservationSnapshot) {
-        var removedReservationRow = htmlForPath[reservationSnapshot.key()];
-        //logelement.remove();
-        delete htmlForPath[reservationSnapshot.key()];
-    }
-    
-    var reservationListView = myDataRef.limitToLast(10);
-    
-    // Add a callback to handle when a new reservation is added.
-    reservationListView.on('child_added', function (newReservationSnapshot, prevReservationName) {
-        handleReservationAdded(newReservationSnapshot, prevReservationName);
-    });
-  
-    // Add a callback to handle when a reservation is removed
-    reservationListView.on('child_removed', function (oldReservationSnapshot) {
-      handleReservationRemoved(oldReservationSnapshot);
-    });
-    
-      // Add a callback to handle when a reservation changes or moves positions.
-      var changedCallback = function (reservationSnapshot, prevReservationName) {
-        handleReservationRemoved(reservationSnapshot);
-        handleReservationAdded(reservationSnapshot, prevReservationName);
-      };
-      reservationListView.on('child_moved', changedCallback);
-      reservationListView.on('child_changed', changedCallback);
-});
+]);
--- js/submit.js
+++ js/submit.js
@@ -1,104 +1,28 @@
-$(document).ready(function() {
-    //reference to the firebase location
-    var myDataRef = new Firebase('https://neutral-cycle.firebaseio.com/');
-    
-    // Keep a mapping of firebase locations to HTML elements, so we can
-    // move / remove elements as necessary.
-    var htmlForPath = {};
-     
-    // Helper function that takes a new score snapshot and adds an appropriate row to our leaderboard table.
-    function handleReservationAdded(reservationSnapshot, prevReservationName) {
-        
-        var data = reservationSnapshot.val();
-        
-        var date        = data.date;      
-        var email       = data.email;     
-        var first_name  = data.first_name;
-        var last_name   = data.last_name; 
-        var phone       = data.phone;  
-        
-        // CREATE ELEMENTS MESSAGE & SANITIZE TEXT
-        
-        var logelement = $("<tr>");
-        
-        $(logelement).append("<td>" + first_name + " </td>");
-        $(logelement).append("<td>" + last_name + " </td>");
-        $(logelement).append("<td>" + date + " </td>");
-        $(logelement).append("<td>" + phone + " </td>");
-        $(logelement).append("<td>" + email + " </td>");
-        
-        // Store a reference to the table row so we can get it again later.
-        htmlForPath[reservationSnapshot.key()] = logelement;
-        
-        if (prevReservationName === null) {
-            $(".log").append(logelement);
-        }
-        else {
-            var lowerScoreRow = htmlForPath[prevReservationName];
-            lowerScoreRow.after(logelement);
+var myApp = angular.module("myApp", ["firebase"]);
+
+myApp.controller('MyController', ['$scope', '$firebase',
+    function($scope, $firebase) {
+        //CREATE A FIREBASE REFERENCE
+        var ref = new Firebase("https://neutral-cycle.firebaseio.com/");
+
+        // GET MESSAGES AS AN ARRAY
+        $scope.reservations = $firebase(ref).$asArray();
+
+        //ADD MESSAGE METHOD
+        $scope.submit = function(event) { 
+//            var datetime = new Date(document.getElementById('pickup_date').innerHTML + " " + document.getElementById('pickup_time').innerHTML + " CST");
+            console.log(datetime);
+
+            //ADD TO FIREBASE
+            $scope.reservations.$add({
+//                date        : datetime.toString(),
+                email       : $scope.email,
+                first_name  : $scope.first_name,
+                last_name   : $scope.last_name,
+                phone       : $scope.phone,
+                rented      : false
+            });
         }
     }
-    // Helper function to handle a reservation object being removed; just removes 
-    // the corresponding table row.
-    function handleReservationRemoved(reservationSnapshot) {
-        var removedReservationRow = htmlForPath[reservationSnapshot.key()];
-        //logelement.remove();
-        delete htmlForPath[reservationSnapshot.key()];
-    }
-    
-    var reservationListView = myDataRef.limitToLast(10);
-    
-    // Add a callback to handle when a new reservation is added.
-    reservationListView.on('child_added', function (newReservationSnapshot, prevReservationName) {
-        handleReservationAdded(newReservationSnapshot, prevReservationName);
-    });
-  
-    // Add a callback to handle when a reservation is removed
-    reservationListView.on('child_removed', function (oldReservationSnapshot) {
-      handleReservationRemoved(oldReservationSnapshot);
-    });
-    
-      // Add a callback to handle when a reservation changes or moves positions.
-      var changedCallback = function (reservationSnapshot, prevReservationName) {
-        handleReservationRemoved(reservationSnapshot);
-        handleReservationAdded(reservationSnapshot, prevReservationName);
-      };
-      reservationListView.on('child_moved', changedCallback);
-      reservationListView.on('child_changed', changedCallback);
-        
-    function generateKey(){
-        var text = "";
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+]);
 
-        for( var i=0; i < 5; i++ )
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-        return text;
-    }
-    
-    function submit(){
-        var key = generateKey();
-        var valid = $('#rental').valid();
-        
-        //validate form
-        if (!valid) {
-            return;
-        }
-        
-        //convert form into array
-        var rental = $("#rental").serializeArray();
-        var datetime = new Date(rental[4].value + " " + rental[5].value + " CST");
-
-        var resRef = myDataRef.child(key);
-        resRef.setWithPriority({ 
-            date        : datetime.toString(),
-            email       : rental[2].value,
-            first_name  : rental[0].value,
-            last_name   : rental[1].value,
-            phone       : rental[3].value,
-            rented      : false
-        }, Date.parse(datetime.toString()));
-    }
-    
-    $("#butts").on("click", submit);
-});
