85df876a48791bc83ca6da79a9a9169b6715a0a3
Lawrence Humphrey <lawrencehumphrey@wirelessprvnat-172-17-179-225.near.illinois.edu>
2015-03-02 16:25:12 -0600

    routing works better

--- js/controllers.js
+++ js/controllers.js
@@ -1,16 +1,33 @@
 var myAppControllers = angular.module('myAppControllers', ['firebase']);
+//CREATE A FIREBASE REFERENCE
+var ref = new Firebase("https://neutral-cycle.firebaseio.com/");
 
 myAppControllers.controller('MGMTFullCtrl', ['$scope', '$firebase',
     function($scope, $firebase) {
-        //CREATE A FIREBASE REFERENCE
-        var ref = new Firebase("https://neutral-cycle.firebaseio.com/");
 
         // GET MESSAGES AS AN ARRAY
         $scope.reservations = $firebase(ref).$asArray();
-        console.log($scope.reservations);
     }]);
 
-myAppControllers.controller('MGMTSingleCtrl', ['$scope', '$routeParams',
-    function($scope, $routeParams) {
-        $scope.resID = $routeParams.resID;
-    }]);
+myAppControllers.controller('MGMTSingleCtrl', ['$scope', '$firebase', '$routeParams',
+    function($scope, $firebase, $routeParams) {
+        $scope.resId = $routeParams.resId;
+//        var resRef = ref + $routeParams.resId;
+//        $scope.userRes = $firebase(resRef).$asObject();
+        
+        //$scope.first_name = userRef.first_name;
+        //$scope.last_name = userRef.last_name;
+        //$scope.email = userRef.email;
+        //$scope.phone = userRef.phone;
+        //$scope.date = userRef.date;
+        
+        var resRef = new Firebase(ref + "/" + $routeParams.resId);
+//        console.log(resRef);
+        $scope.userRes = $firebase(resRef).$asObject();
+        
+        //$scope.first_name = userRef.first_name;
+        //$scope.last_name = userRef.last_name;
+        //$scope.email = userRef.email;
+        //$scope.phone = userRef.phone;
+        //$scope.date = userRef.date;
+    }]); 
