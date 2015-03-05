8c927d2b11f6424b8d918ea3a53ac419d983235b
Noah Laferty <noah.lafferty@gmail.com>
2015-03-05 17:28:59 -0600

    something

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ myApp.config(['$routeProvider', '$locationProvider',
     function($routeProvider, $locationProvider) {
         $locationProvider.html5Mode(true);
         $routeProvider.
-        when('/', {
+        when('/rentals', {
             tempateUrl: '/templates/rental.html',
             controller: 'RentalCtrl'
         }).
