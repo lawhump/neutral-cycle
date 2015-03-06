ca46d6e0c6f1a596d93dd31b243a5dd45ed8c70e
Lawrence Humphrey <lawrencehumphrey@wirelessprvnat-172-17-31-143.near.illinois.edu>
2015-03-05 18:22:33 -0600

    routing doesn't work what happened

--- js/controllers.js
+++ js/controllers.js
@@ -19,22 +19,6 @@ myAppControllers.controller('RentalCtrl', ['$scope', '$firebase',
 myAppControllers.controller('MGMTSingleCtrl', ['$scope', '$firebase', '$routeParams',
     function($scope, $firebase, $routeParams) {
         $scope.resId = $routeParams.resId;
-//        var resRef = ref + $routeParams.resId;
-//        $scope.userRes = $firebase(resRef).$asObject();
-        
-        //$scope.first_name = userRef.first_name;
-        //$scope.last_name = userRef.last_name;
-        //$scope.email = userRef.email;
-        //$scope.phone = userRef.phone;
-        //$scope.date = userRef.date;
-        
         var resRef = new Firebase(ref + "/" + $routeParams.resId);
-//        console.log(resRef);
         $scope.userRes = $firebase(resRef).$asObject();
-        
-        //$scope.first_name = userRef.first_name;
-        //$scope.last_name = userRef.last_name;
-        //$scope.email = userRef.email;
-        //$scope.phone = userRef.phone;
-        //$scope.date = userRef.date;
     }]); 
